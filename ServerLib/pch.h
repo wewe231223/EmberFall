5297816430d1b3784091704fc1aa451a0f433b17
KSJTUK <lastbabo2480@tukorea.ac.kr>
2025-01-09 23:43:13 +0900

    pch에 기본적인 내용들 추가

---
+++ ServerLib/pch.h
@@ -0,0 +1,48 @@
+﻿#pragma once
+
+// Windows && C headers
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+#include <Windows.h>
+
+#undef max
+#undef min
+
+// I/O header
+#include <iostream>
+#include <fstream>
+
+// container
+#include <array>
+#include <vector>
+#include <unordered_map>
+
+// utils
+#include <chrono>
+#include <variant>
+#include <concepts>
+#include <ranges>
+#include <numeric>
+#include <algorithm>
+
+// concurrency support
+#include <thread>
+#include <atomic>
+#include <mutex>
+#include <condition_variable>
+
+// concurrency container
+#include <concurrent_unordered_map.h>
+#include <concurrent_queue.h>
+
+//#include "Util.h"
+//#include "Types.h"
+//#include "Constants.h"
+//#include "OverlappedEx.h"
+//#include "Crash.h"
+
+#include <latch>
