ccedef48d62b8fa59690015f4507a6148e7542c0
KSJTUK <lastbabo2480@tukorea.ac.kr>
2025-01-18 21:39:57 +0900

    메모리 풀 클래스 작성

--- ServerLib/MemoryPool.h
+++ ServerLib/MemoryPool.h
@@ -1,14 +1,38 @@
 #pragma once
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// MemoryPool.cpp
+// 2025 - 01 - 18 김성준 : new delete 연산을 최대한 줄이기 위한 메모리풀
+//      AllocMemBlocks 함수를 통해 미리 할당. Push, Pop을 통해 메모리를 얻어온다.
+// 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 class MemoryPool {
+public:
+    using MemBlockIdxType = size_t;
+
+    inline static size_t MIN_BLOCK_SIZE = 32;
+
 public:
     MemoryPool();
     ~MemoryPool();
 
+    MemoryPool(const MemoryPool&) = delete;
+    MemoryPool(MemoryPool&&) noexcept = delete;
+    MemoryPool& operator=(const MemoryPool&) = delete;
+    MemoryPool& operator=(MemoryPool&&) noexcept = delete;
+
 public:
-    void AllocMemBlocks(size_t memBlockSize, size_t memBlockCount);
+    bool AllocMemBlocks(size_t memBlockSize, size_t memBlockCount);
+
+    void* Pop();
+    bool Push(void* ptr);
 
 private:
     void* mBasePtr{ nullptr };
+    size_t mMemBlockSize{ };
+    size_t mMemBlockCount{ };
+    Concurrency::concurrent_queue<MemBlockIdxType> mAvailableIndices{ };
 };
 
