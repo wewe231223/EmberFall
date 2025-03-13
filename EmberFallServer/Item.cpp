53ac5096b3d6ffe48bc18f637a9e5a9703e6ef5b
KSJTUK <lastbabo2480@tukorea.ac.kr>
2025-03-13 10:53:52 +0900

    아이템 함수 변경

--- EmberFallServer/Item.cpp
+++ EmberFallServer/Item.cpp
@@ -3,15 +3,30 @@
 #include "GameObject.h"
 
 void Item::UseItem(ItemTag tag, const std::shared_ptr<class GameObject>& obj) {
+    switch (tag) {
+    case ItemTag::ITEM_POTION:
+        Potion::UseItem(obj);
+        break;
 
+    case ItemTag::ITEM_HOLYWATER:
+        HolyWater::UseItem(obj);
+        break;
+
+    case ItemTag::ITEM_CROSS:
+        Cross::UseItem(obj);
+        break;
+
+    default:
+        break;
+    }
 }
 
 void Potion::UseItem(const std::shared_ptr<class GameObject>& obj) {
-
+    obj->RestoreHealth(10.0f);
 }
 
 void HolyWater::UseItem(const std::shared_ptr<class GameObject>& obj) {
-
+    
 }
 
 void Cross::UseItem(const std::shared_ptr<class GameObject>& obj) {
