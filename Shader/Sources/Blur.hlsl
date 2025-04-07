ae34570969c47b3df06f55115e005689b4321348
Jeong071 <rldudwjd123@naver.com>
2025-04-07 15:51:51 +0900

    blur셰이더 코드 if문 -> for문 기반으로 변경

--- Shader/Sources/Blur.hlsl
+++ Shader/Sources/Blur.hlsl
@@ -3,11 +3,6 @@ Texture2D gInput : register(t0);
 
 RWTexture2D<float4> gRWOutput : register(u0);
 
-
-
-
-
-
 static float gGaussianBlurMask1D[11] = { 0.05f, 0.05f, 0.1f, 0.1f, 0.1f, 0.2f, 0.1f, 0.1f, 0.1f, 0.05f, 0.05f };
 
 groupshared float4 gGroupSharedCache[256 + 5 + 5];
@@ -15,19 +10,24 @@ groupshared float4 gGroupSharedCache[256 + 5 + 5];
 [numthreads(256, 1, 1)]
 void HorzBlur_CS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
 {
-    if (groupThreadID.x < 5)
-    {
-        int x = max(dispatchThreadID.x - 5, 0);
-        gGroupSharedCache[groupThreadID.x] = gInput[int2(x, dispatchThreadID.y)];
-    }
-    else if (groupThreadID.x >= (256 - 5))
-    {
-        int x = min(dispatchThreadID.x + 5, gInput.Length.x - 1);
-        gGroupSharedCache[groupThreadID.x + (2 * 5)] = gInput[int2(x, dispatchThreadID.y)];
-    }
-    gGroupSharedCache[groupThreadID.x + 5] = gInput[min(dispatchThreadID.xy, gInput.Length.xy - 1)];
+   
+    
 
-    GroupMemoryBarrierWithGroupSync();
+    [unroll]
+    for (int i = 0; i < 5; ++i) {
+        gGroupSharedCache[groupThreadID.x] = gInput[int2(max(dispatchThreadID.x - 5, 0), dispatchThreadID.y)];
+    }
+    
+    gGroupSharedCache[groupThreadID.x + 5] = gInput[min(dispatchThreadID.xy, gInput.Length.xy - 1)];
+    
+    [unroll]
+    for (int i = 261; i <= 265; ++i) {
+        gGroupSharedCache[groupThreadID.x + (2 * 5)] = gInput[int2(min(dispatchThreadID.x + 5, gInput.Length.x - 1), dispatchThreadID.y)];
+
+    }
+    
+    
+        GroupMemoryBarrierWithGroupSync();
 
     float4 color = float4(0, 0, 0, 0);
     [unroll]
@@ -40,18 +40,26 @@ void HorzBlur_CS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : 
 [numthreads(1, 256, 1)]
 void VertBlur_CS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
 {
-    if (groupThreadID.y < 5)
-    {
-        int y = max(dispatchThreadID.y - 5, 0);
-        gGroupSharedCache[groupThreadID.y] = gInput[int2(dispatchThreadID.x, y)];
+    
+    
+    [unroll]
+    for (int i = 0; i < 5; ++i) {
+        gGroupSharedCache[groupThreadID.y] = gInput[int2(dispatchThreadID.x, max(dispatchThreadID.y - 5, 0))];
     }
-    else if (groupThreadID.y >= 256 - 5)
-    {
-        int y = min(dispatchThreadID.y + 5, gInput.Length.y - 1);
-        gGroupSharedCache[groupThreadID.y + (2 * 5)] = gInput[int2(dispatchThreadID.x, y)];
-    }
+    
     gGroupSharedCache[groupThreadID.y + 5] = gInput[min(dispatchThreadID.xy, gInput.Length.xy - 1)];
 
+    
+    [unroll]
+    for (int i = 261; i <= 265; ++i) {
+        gGroupSharedCache[groupThreadID.y + (2 * 5)] = gInput[int2(dispatchThreadID.x, min(dispatchThreadID.y + 5, gInput.Length.y - 1))];
+
+    }
+    
+    
+
+    
+    
     GroupMemoryBarrierWithGroupSync();
 
     float4 color = float4(0, 0, 0, 0);
