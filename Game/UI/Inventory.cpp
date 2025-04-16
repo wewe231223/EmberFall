e7683677a7e88f33854ed16e4b48c5b6e16a125b
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2025-04-17 01:18:00 +0900

    위치, 크기 조절

--- Game/UI/Inventory.cpp
+++ Game/UI/Inventory.cpp
@@ -2,17 +2,29 @@
 #include "Inventory.h"
 
 void Inventory::Init(std::shared_ptr<Canvas> canvas, UINT base, UINT frame, UINT health, UINT cross, UINT holyWater) {
-	for(auto i = 0; i < 3; ++i) {
+	float screenCenterX = Config::WINDOW_WIDTH<float> / 2.f;
+	float screenBottomY = Config::WINDOW_HEIGHT<float>;
+	
+	constexpr float rectWidth = 100.f;
+	constexpr float rectHeight = 100.f;
+	
+	float totalWidth = rectWidth * 3.f;
+	float startX = screenCenterX - totalWidth / 2.f;
+	float posY = screenBottomY - rectHeight; 
+
+	for (int i = 0; i < 3; ++i) {
 		auto& pair = mItem[i];
 		pair.first = canvas->CreateCanvasObject();
 		pair.second = canvas->CreateCanvasObject();
 
 		pair.first.ChangeImage(frame);
-		pair.first.GetRect() = { 760.f + i * 200.f, 880.f, 200.f, 200.f };
-
 		pair.second.ChangeImage(base);
-		pair.second.GetRect() = { 760.f + i * 200.f, 880.f, 200.f, 200.f };
+
+		float x = startX + i * rectWidth;
+		pair.first.GetRect() = { x, posY, rectWidth, rectHeight };
+		pair.second.GetRect() = { x, posY, rectWidth, rectHeight };
 	}
+
 }
 
 void Inventory::Update() {
