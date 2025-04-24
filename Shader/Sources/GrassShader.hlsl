144ac0145ccbdd0ca5f90d2b7a5d72ed262ee1f8
SeungBeom Kim@Desktop <64590456+wewe231223@users.noreply.github.com>
2025-04-24 21:41:47 +0900

    풀 랜덤 컬링

--- Shader/Sources/GrassShader.hlsl
+++ Shader/Sources/GrassShader.hlsl
@@ -8,14 +8,9 @@ cbuffer CameraCB : register(b0)
     int isShadow;
 };
 
-
-cbuffer TerrainGlobal : register(b1)
+cbuffer Time
 {
-    int globalWidth;
-    int globalHeight;
-    float gridSpacing;
-    float minX;
-    float minZ;
+    uint globalTime; // milliseconds 
 };
 
 cbuffer MaterialIndex : register(b2)
@@ -45,10 +40,9 @@ struct GrassPosition
 };
 
 
-StructuredBuffer<float3> TerrainVertices : register(t0);
-StructuredBuffer<GrassPosition> grassVertices : register(t1);
-StructuredBuffer<MaterialConstants> materialConstants : register(t2);
-Texture2D textures[1024] : register(t3);
+StructuredBuffer<GrassPosition> grassVertices : register(t0);
+StructuredBuffer<MaterialConstants> materialConstants : register(t1);
+Texture2D textures[1024] : register(t2);
 
 SamplerState pointWrapSampler : register(s0);
 SamplerState pointClampSampler : register(s1);
@@ -65,8 +59,11 @@ struct Payload
 };
 
 #define GRASS_GRID_COUNT 2500
-#define GRASS_PER_GRID 512
 #define GRASS_PER_DISPATCH 16
+
+// 이 상수를 통해 그리드 당 풀의 개수를 제어함. 
+// 이 상수는 반드시 GRASS_PER_DISPATCH 의 배수여야 함 ( GRASS_PER_DISPATCH <= 16 ) 
+#define GRASS_PER_GRID 400
 #define GRASS_CULL_DISTANCE 200.0f
 #define GRASS_COUNT GRASS_PER_GRID * GRASS_GRID_COUNT
 #define MAX_VERTEX_COUNT (GRASS_PER_DISPATCH * 8)
@@ -101,6 +98,7 @@ void mainAS(uint3 groupId : SV_GroupID)
         pl.baseIndex = 0;
         pl.culled = true; 
     }
+    
 
     DispatchMesh(GRASS_PER_GRID / GRASS_PER_DISPATCH, 1, 1, pl); 
 }
@@ -115,32 +113,49 @@ struct VSOutput
     uint texIndex : TEXID;
 };
 
+
+float hash(uint x)
+{
+    x ^= x >> 17;
+    x *= 0xed5ad4bb;
+    x ^= x >> 11;
+    x *= 0xac4c1b51;
+    x ^= x >> 15;
+    x *= 0x31848bab;
+    x ^= x >> 14;
+    return frac(x * (1.0 / 4294967296.0));
+}
+
 [outputtopology("triangle")]
 [numthreads(GRASS_PER_DISPATCH, 1, 1)]
 void mainMS(
     uint3 threadId : SV_DispatchThreadID,
     uint3 groupThreadId : SV_GroupThreadID,
+    uint3 groupId : SV_GroupID,
     in payload Payload pl,
     out indices uint3 outIndices[MAX_INDEX_COUNT],
     out vertices VSOutput outVerts[MAX_VERTEX_COUNT])
 {
     const uint localId = groupThreadId.x;
-
     const uint groupIndex = threadId.x / GRASS_PER_DISPATCH;
     const uint offsetInGrid = localId;
     // baseIndex + (Mesh Shader Group Id * GRASS_PER_DISPATCH) + localId
     const uint grassIndex = pl.baseIndex + groupIndex * GRASS_PER_DISPATCH + offsetInGrid;
 
-    SetMeshOutputCounts(pl.culled ? 0 : 8, pl.culled ? 0 : 12);
+   
+    bool shouldCull = pl.culled;
+    SetMeshOutputCounts(shouldCull ? 0 : 8, shouldCull ? 0 : 12);
 
-    if (pl.culled)
+    if (shouldCull)
         return;
-
+                                                                                                    
     GrassPosition grass = grassVertices[grassIndex];
     
+    float rand = hash(grassIndex);
     
     float3 basePos = grass.position;
     float halfSize = grass.scale * 0.6f;
+   // rand < 0.5 ? halfSize = 0.f : halfSize = halfSize; 
     basePos.y += halfSize;
 
     // 고정 방향 기반으로 mesh 잔디 구성
